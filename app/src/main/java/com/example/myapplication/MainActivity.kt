d3068d02ec6fb098d2727a20345d59530f77baa6
Robert-MI <mirzoyan.rob@gmail.com>
2023-12-02 13:21:11 +0400

    reading data from json file

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,17 +3,12 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -35,15 +30,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-var lst = listOf(
-    //description and photopath will be changed
-    Coffee(1,"Americano",800, "to be added", "subject to change"),
-    Coffee(2,"Cappuccino",1000, "to be added", "subject to change"),
-    Coffee(3,"Latte",1100, "to be added", "subject to change"),
-    Coffee(4,"Black Coffee",700, "to be added", "subject to change"),
-    Coffee(5,"Frappuccino",1200, "to be added", "subject to change")
-)
 var TotalPrice = 0
 var CurrentCoffee = Coffee()
 
@@ -63,26 +49,6 @@ fun CustomNavigationView() {
         }
     }
 }
-
-@Composable
-fun TotalAmount(total: Int) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {

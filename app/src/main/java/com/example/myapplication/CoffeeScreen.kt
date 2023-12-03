8058ae4eeb9a657dfe1bb155e1296c7901750ba2
Sevada <sevadaiskandaryan@gmail.com>
2023-12-04 00:06:35 +0400

    added top menu

--- app/src/main/java/com/example/myapplication/CoffeeScreen.kt
+++ app/src/main/java/com/example/myapplication/CoffeeScreen.kt
@@ -1,8 +1,28 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 
@@ -10,9 +30,73 @@ import androidx.navigation.NavController
 fun CoffeeScreen(
     navController: NavController
 ){
-    Button(onClick = {
-        navController.navigate(route = "CoffeeList")
-    }) {
-        Text(text = CurrentCoffee.name)
+    CoffeScreenTopMenu(navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CoffeScreenTopMenu(navController: NavController) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Description",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate(route = "CoffeeList") }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { innerPadding ->
+        CoffeeScreenScrollContent(innerPadding, navController)
+    }
+}
+
+@Composable
+fun CoffeeScreenScrollContent(innerPadding: PaddingValues, navController: NavController) {
+    val coffeeList: List<Coffee> = getCoffeeList()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(innerPadding),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+//        Button(onClick = {
+//            navController.navigate(route = "CoffeeList")
+//        }) {
+//            Text(text = CurrentCoffee.name)
+//        }
+
+        Text(
+            text = CurrentCoffee.name,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.google.gson.Gson
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +34,17 @@ class MainActivity : ComponentActivity() {
 }
 var TotalPrice = 0
 var CurrentCoffee = Coffee()
+
+@Composable
+fun getCoffeeList() : List<Coffee>{
+    val file_name = "coffees.json"
+    val json_string = LocalContext.current.assets.open(file_name).bufferedReader().use {
+        it.readText()
+    }
+    val gson = Gson()
+    val coffeeList: List<Coffee> = gson.fromJson(json_string, Array<Coffee>::class.java).toList()
+    return coffeeList
+}
 
 @Composable
 fun CustomNavigationView() {
